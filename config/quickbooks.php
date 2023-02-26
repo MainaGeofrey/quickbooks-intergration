4335a9db6dce13ac0f26bf3b7127d7ef6c691f0b
jeff <mainajfry@gmail.com>
2023-02-26 14:15:43 +0300

    auth config

--- config/quickbooks.php
+++ config/quickbooks.php
@@ -1,12 +1,12 @@
 <?php
 return array(
-    'authorizationRequestUrl' => 'https://appcenter.intuit.com/connect/oauth2',
-    'tokenEndPointUrl' => 'https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer',
-    'client_id' => 'ABXBLQ54LCA8qVFquSWWCR3155HTpAd2LSLISJfVWCGKDHgVjH',
-    'client_secret' => 'AJO5lc8Oqt1z7XerY6gcoXmPJEJiuT5tHqD7tId1',
+    'authorizationRequestUrl' => '',
+    'tokenEndPointUrl' => '',
+    'client_id' => '',
+    'client_secret' => '',
     'oauth_scope' => 'com.intuit.quickbooks.accounting openid profile email phone address',
     'oauth_redirect_uri' => 'http://localhost:3000/callback.php',
-    'QBORealmID' => "4620816365281741020",
-    'refresh_token' => "AB116859556504js9Ng7jUR3VrLy1XTMlV8qvRz4QeJtaY7tjv",
+    'QBORealmID' => "",
+    'refresh_token' => "",
 )
 ?>
