dab12ebb6319954994cc9c84f8e39970c4f9009b
jeff <mainajfry@gmail.com>
2023-02-27 17:46:50 +0300

    change

--- app/Helpers/Utils.php
+++ app/Helpers/Utils.php
@@ -9,8 +9,7 @@ use Illuminate\Support\Facades\Log;
 class Utils{
     static public  function getApiUser($request)
 	{
-        return 3;
-		//return DB::table('api_tokens')->where('api_token', hash('sha256', $request->bearerToken()))->first()->user_id;
+		return DB::table('api_tokens')->where('api_token', hash('sha256', $request->bearerToken()))->first()->user_id;
 	}
 }
 
