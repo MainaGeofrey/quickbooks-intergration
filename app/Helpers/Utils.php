c425b2a914877fc962ce1cf837f72df6170e8804
jeff <mainajfry@gmail.com>
2023-02-28 15:48:00 +0300

    auth hederers debug

--- app/Helpers/Utils.php
+++ app/Helpers/Utils.php
@@ -9,8 +9,6 @@ use Illuminate\Support\Facades\Log;
 class Utils{
     static public  function getApiUser($request)
 	{
-        print_r($request->bearerToken());
-        print_r(DB::table('api_tokens')->where('api_token', hash('sha256', $request->bearerToken()))->first());
 		return DB::table('api_tokens')->where('api_token', hash('sha256', $request->bearerToken()))->first()->user_id;
 	}
 }
