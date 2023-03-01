ca5caedb664e77aef00ceaaae31f37895419d7c0
jeff <mainajfry@gmail.com>
2023-03-01 13:07:35 +0300

    change

--- config/quickbooks.php
+++ config/quickbooks.php
@@ -2,11 +2,11 @@
 return array(
     'authorizationRequestUrl' => 'https://appcenter.intuit.com/connect/oauth2',
     'tokenEndPointUrl' => 'https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer',
-    'client_id' => '',
-    'client_secret' => '',
+    'client_id' => 'ABXBLQ54LCA8qVFquSWWCR3155HTpAd2LSLISJfVWCGKDHgVjH',
+    'client_secret' => 'AJO5lc8Oqt1z7XerY6gcoXmPJEJiuT5tHqD7tId1',
     'oauth_scope' => 'com.intuit.quickbooks.accounting openid profile email phone address',
     'oauth_redirect_uri' => 'http://localhost:3000/callback.php',
-    'QBORealmID' => "",
-    'refresh_token' => "",
+    'QBORealmID' => "4620816365281741020",
+    'refresh_token' => "AB11686304479HmMl5nWo1C2BBo4AJ9TTPVCNSbj9hZv80qLct",
 )
 ?>
